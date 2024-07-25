1b28c53a2f95b85eca85f717da0b0994029445da
Madhan <ramadhaniking2015@gmail.com>
2024-07-25 14:24:07 +0800

    Membuat Interaktivitas Soal

--- resources/views/latihan/latihan.blade.php
+++ resources/views/latihan/latihan.blade.php
@@ -11,6 +11,7 @@
     
 
     $data_soal = $_SESSION['data_soal'];
+    $data_dari_json = json_encode($soal->data_soal);
     $soal_sekarang = $_SESSION['soal_sekarang'];
     $jawaban = $_SESSION['jawaban'];
     $nilai = $_SESSION['nilai'];
@@ -38,6 +39,8 @@
     
 ?>
 
+<p id="kumpulan-soal" hidden>{{$data_dari_json}}</p>
+
 <div class="mt-3">
     <h1>Latihan</h1>
 
@@ -61,31 +64,28 @@
                 {{-- Menampilkan Soal --}}
                 
                 <div class="card-body"> 
-                    <p>{{$soal_sekarang+1 . '.  ' . $soal['pertanyaan']}}</p>
+                    <p id="soal"></p>
                     <ol class="mt-4" type="a">
-                        @foreach ($soal['pilihan'] as $pilihan)
-                            <li>{{$pilihan['teks']}}</li>
-                        @endforeach
+                        <li id="pilihan-1"></li>
+                        <li id="pilihan-2"></li>
+                        <li id="pilihan-3"></li>
+                        <li id="pilihan-4"></li>
                     </ol>
                 </div>
                
             </div>
-        <form action="" method="POST">
-            @csrf
-            @method('PUT')
             <div class="row">
                 {{-- Navigasi Next dan Prev --}}
                 <div class="col-4">
-                    <input class="btn btn-primary mt-3" type="submit" name="prev" value="Sebelumnya" {{$soal_sekarang > 0 ? '' : 'disabled'}}>
+                    <input class="btn btn-primary mt-3" type="submit" name="prev" value="Sebelumnya" id="prev" onclick="prev()">
                 </div>
                 <div class="col-4 text-center">
                     <button class="btn btn-warning mt-3">Ragu</button> 
                 </div>
                 <div class="col-4 text-end">
-                    <input class="btn btn-primary mt-3" type="submit" name="next" value="Selanjutnya" {{$soal_sekarang < count($soal) ? '' : 'disabled'}}>
+                    <input class="btn btn-primary mt-3" type="submit" name="next" value="Selanjutnya" id="next" onclick="next()">
                 </div>
             </div>
-        </form>
         </div>
         <div class="col-4 p-2">
             <div class="card shadow">
@@ -125,7 +125,81 @@
 
 <script>
     // Mengambil kumpulan soal
-    console.log($soal);
+    //console.log($soal);
+    const $soal_soal = document.getElementById('kumpulan-soal');
+    console.log('Hello World');
+    
+    // Mengubah data JSON dari HTML ke JSON Javascript, perlu dua kali parse
+    const $data_soal = JSON.parse(JSON.parse($soal_soal.innerHTML));
+
+    // Cek isi data Soal di Console Log
+    console.log($data_soal)
+    console.log($data_soal[0]['pilihan'][0]['teks'])
+
+    // Menampilkan soal pada halaman
+    const $soal = document.getElementById('soal');
+    const $pilihan_1 = document.getElementById('pilihan-1');
+    const $pilihan_2 = document.getElementById('pilihan-2');
+    const $pilihan_3 = document.getElementById('pilihan-3');
+    const $pilihan_4 = document.getElementById('pilihan-4');
+    var $soal_sekarang = 0;
+
+    function hapus_soal(){
+        $soal.innerHTML = ''
+        $pilihan_1.innerHTML = ''
+        $pilihan_2.innerHTML = ''
+        $pilihan_3.innerHTML = ''
+        $pilihan_4.innerHTML = ''
+    }
+    
+    function isi_soal($nomor){
+        $soal.innerHTML = $data_soal[$nomor]['pertanyaan']
+        $pilihan_1.innerHTML = $data_soal[$nomor]['pilihan'][0]['teks']
+        $pilihan_2.innerHTML = $data_soal[$nomor]['pilihan'][1]['teks']
+        $pilihan_3.innerHTML = $data_soal[$nomor]['pilihan'][2]['teks']
+        $pilihan_4.innerHTML = $data_soal[$nomor]['pilihan'][3]['teks']
+    }
+
+    isi_soal($soal_sekarang)
+
+     // Matikan Tombol
+     function nav_tombol(){
+        console.log($soal_sekarang)
+        if($soal_sekarang == 4){
+            document.getElementById('next').disabled = true;
+            document.getElementById('prev').disabled = false;
+        }else if($soal_sekarang == 0){
+            document.getElementById('prev').disabled = true;
+            document.getElementById('next').disabled = false;
+        }else{
+            document.getElementById('prev').disabled = false;
+            document.getElementById('next').disabled = false;
+        }
+    }
+
+    nav_tombol()
+
+    
+    // Navigasi
+    function next(){
+        console.log('Next')
+        $soal_sekarang++;
+        hapus_soal()
+        isi_soal($soal_sekarang)
+        nav_tombol()
+        
+    }
+    function prev(){
+        console.log('Prev')
+        $soal_sekarang--;
+        hapus_soal()
+        isi_soal($soal_sekarang)
+        nav_tombol()
+    }
+    
+   
+    
+
 </script>
 
 @endsection
