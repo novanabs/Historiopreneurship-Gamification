ae28270229d6424c4682f34dab09305fcf8e7dc3
Madhan <ramadhaniking2015@gmail.com>
2024-08-15 11:11:35 +0800

    Fitur Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,9 +34,23 @@
                 <div class="card p-4">
                     <div class="card-body">
                         <h5 class="card-title">Profil</h5>
-                        <p class="card-text">Nama : {{ auth()->user()->nama_lengkap }}</p>
+                        <p class="card-text mt-5">Nama : {{ auth()->user()->nama_lengkap }}</p>
                         <p class="card-text">Kelas : </p>
                         <p class="card-text">Sekolah :</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Badge -->
+            <div class="col-md-6 mb-4">
+                <div class="card p-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Badge</h5>
+                        <p class="card-text">Perolehan Badge</p>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><img src="{{ asset('img/aktif.png') }}" alt="aktif" width="100px"></li>
+                            <li class="list-inline-item"><img src="{{ asset('img/rajin.png') }}" alt="rajin" width="100px"></li>
+                            <li class="list-inline-item"><img src="{{ asset('img/cepat.png') }}" alt="cepat" width="100px"></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -49,21 +63,24 @@
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Progress</h5>
-                        <p class="card-text display-5">42%</p>
-                    </div>
-                </div>
-            </div>
-            <!-- Badge -->
-            <div class="col-md-6 mb-4">
-                <div class="card p-4">
-                    <div class="card-body">
-                        <h5 class="card-title">Badge</h5>
-                        <p class="card-text">3 Badge Dimiliki</p>
-                        <ul class="list-unstyled">
-                            <li>Badge 1</li>
-                            <li>Badge 2</li>
-                            <li>Badge 3</li>
-                        </ul>
+                        <br><br>
+
+                        <h6 class="text-start">Informasi Umum</h6>
+                        <div class="progress rounded" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated text-dark" style="width:100%" id="status_bar"></div>
+                        </div> 
+
+                        <h6 class="text-start mt-3">1. Kesejarahan</h6>
+                        <div class="progress rounded" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" style="width:57%" id="status_bar"></div>
+                        </div> 
+
+                        <h6 class="text-start mt-3">2. KWU & Kepariwisataan</h6>
+                        <div class="progress rounded" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" style="width:23%" id="status_bar"></div>
+                        </div> 
+
+                        
                     </div>
                 </div>
             </div>
@@ -74,13 +91,68 @@
                         <h5 class="card-title">Leaderboard</h5>
                         <p class="card-text">Peringkat 5</p>
                         <p class="card-text">Top 10 Siswa:</p>
-                        <ul class="list-unstyled">
-                            <li>1. Siswa A</li>
-                            <li>2. Siswa B</li>
-                            <li>3. Siswa C</li>
-                            <li>4. Siswa D</li>
-                            <li>5. Siswa E</li>
-                        </ul>
+                        <table class="table table-bordered table-stripped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Poin</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <td>Madhan</td>
+                                    <td>2134</td>
+                                </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <td>Salman</td>
+                                    <td>2130</td>
+                                </tr>
+                                <tr>
+                                    <th>3</th>
+                                    <td>Husna</td>
+                                    <td>2122</td>
+                                </tr>
+                                <tr>
+                                    <th>4</th>
+                                    <td>Iif Alifah</td>
+                                    <td>2120</td>
+                                </tr>
+                                <tr>
+                                    <th>5</th>
+                                    <td>Rifqi</td>
+                                    <td>2103</td>
+                                </tr>
+                                <tr>
+                                    <th>6</th>
+                                    <td>Ari Yono</td>
+                                    <td>2078</td>
+                                </tr>
+                                <tr>
+                                    <th>7</th>
+                                    <td>Fadhil</td>
+                                    <td>2068</td>
+                                </tr>
+                                <tr>
+                                    <th>8</th>
+                                    <td>Fajar</td>
+                                    <td>2000</td>
+                                </tr>
+                                <tr>
+                                    <th>9</th>
+                                    <td>Saipul</td>
+                                    <td>1994</td>
+                                </tr>
+                                <tr>
+                                    <th>10</th>
+                                    <td>Fuad</td>
+                                    <td>1800</td>
+                                </tr>
+                            </tbody>
+
+                        </table>
                     </div>
                 </div>
             </div>
